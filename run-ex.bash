f5b85287d2ed1a78d32bffad8c198f5bfd2f9065
Ryan S <ryanssteles@gmail.com>
2024-06-28 15:38:13 -0300

    Adicionando icons

--- run-ex.bash
+++ run-ex.bash
@@ -7,6 +7,9 @@ YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
+CHECKMARK='\xE2\x9C\x94'
+CROSS='\xE2\x9D\x8C'
+
 code=$1
 testcaseDir=$2
 testcase=$3
@@ -18,7 +21,7 @@ if [ -z "$code" ] || [ -z "$testcaseDir" ]; then
     echo -e "  ../run-ex.bash 03.py testcases"
     echo -e "${YELLOW}or if you want to run a specific testcase:${NC}"
     echo -e "  ../run-ex.bash 03.cpp testcases 1"
-    return
+    exit 1
 fi
 
 lang=${code##*.}
@@ -29,7 +32,7 @@ if ! ls $testcaseDir/*.in 1> /dev/null 2>&1 || ! ls $testcaseDir/*.out 1> /dev/n
     zipfile="$testcaseDir/$prefix.zip"
     if [ -f "$zipfile" ]; then
         echo -e "${BLUE}Extracting $zipfile...${NC}"
-        unzip -o $zipfile -d $testcaseDir
+        unzip -q -o $zipfile -d $testcaseDir
         # Rename files with prefix
         for file in $testcaseDir/*; do
             # If ends with .in or .out
@@ -50,7 +53,6 @@ run_test() {
     local output_file=${input_file%.*}.out
 
     echo -e "${YELLOW}Running test case $input_file...${NC}"
-    echo
 
     if [ $lang = "py" ]; then
         diff_output=$(diff -y --suppress-common-lines <(python3 $code < $input_file) $output_file | expand -t 2)
@@ -59,13 +61,18 @@ run_test() {
     fi
 
     if [ -z "$diff_output" ]; then
-        echo -e "${GREEN}Test case $input_file passed.${NC}"
+        echo -e "${GREEN}$CHECKMARK Test case $input_file passed.${NC}"
     else
-        echo -e "${RED}Test case $input_file failed.${NC}"
+        echo -e "${RED}$CROSS Test case $input_file failed.${NC}"
         echo "$diff_output"
+        echo -e "${RED}Stopping further tests due to failure.${NC}"
+        all_passed=false
+        return 1
     fi
     echo
 }
+
+all_passed=true
 
 if [ $lang = "py" ]; then
     if [ -n "$testcase" ]; then
@@ -73,6 +80,9 @@ if [ $lang = "py" ]; then
     else
         for input_file in $(ls $testcaseDir/$prefix-*.in); do
             run_test $input_file
+            if [ $? -ne 0 ]; then
+                break
+            fi
         done
     fi
 elif [ $lang = "cpp" ]; then
@@ -90,6 +100,9 @@ elif [ $lang = "cpp" ]; then
     else
         for input_file in $(ls $testcaseDir/$prefix-*.in); do
             run_test $input_file
+            if [ $? -ne 0 ]; then
+                break
+            fi
         done
     fi
 
@@ -103,4 +116,9 @@ rm $testcaseDir/$prefix-*.in $testcaseDir/$prefix-*.out $testcaseDir/README.txt
 if [ $lang = "cpp" ]; then
     rm $testcaseDir/main
 fi
-echo -e "${GREEN}All done.${NC}"
+
+if $all_passed; then
+    echo -e "${GREEN}All done. All tests passed.${NC}"
+else
+    echo -e "${RED}Test execution stopped due to failure.${NC}"
+fi
